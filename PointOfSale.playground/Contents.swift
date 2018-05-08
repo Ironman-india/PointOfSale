d62878a700bb82218457c8e4b4eb340b7ecc5f31
HITESH SAVALIYA <hitesh.savaliya@gmail.com>
2018-05-08 09:09:43 +0100

    Test cases: 1. To test creation of namespace "CheckoutSystem" // Red 2. To test api "totalPrice" of "CheckoutSystem" // Red

--- PointOfSale.playground/Contents.swift
+++ PointOfSale.playground/Contents.swift
@@ -1,5 +1,38 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
 
-var str = "Hello, playground"
+
+//MARK: - TEST CASES
+class CheckoutSystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testCheckoutSystemExists() {
+        XCTAssertNotNil(CheckoutSystem(), "Failed: Checkout System can't be nil")
+    }
+    
+    func testTotalPriceOfEmptyCart() {
+        let products: [Any] = [] //TODO: Refactor
+        XCTAssertEqual(CheckoutSystem().totalPrice(products), 0, "Failed: Total price of empty cart should be zero")
+    }
+    
+    /*
+    func testTotalPriceOfSingleItemInCart() {
+        
+    }
+    
+    func testTotalPriceOfMutlipleItemsInCart() {
+    }
+    */
+    
+}
+
+CheckoutSystemTests.defaultTestSuite.run()
